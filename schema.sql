0f441af243769899ac72a00a7618c67b92e7f2a0
Henri <ecch9210@gmail.com>
2021-06-05 20:31:00 -0500

    Schema and seeds added

--- schema.sql
+++ schema.sql
@@ -9,16 +9,16 @@ CREATE TABLE department (
 )
 
 CREATE TABLE role_employee (   
-    id INT PRIMARY KEY
-    title VARCHAR(30)
-    salary DECIMAL 
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL, 
     department_id INT 
 )
 CREATE TABLE employee (   
-    id INT PRIMARY KEY
-    first_name VARCHAR(30)
-    last_name VARCHAR(30)
-    role_id INT
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
     manager_id INT 
 )
 
---
+++ seeds.sql
@@ -0,0 +1,14 @@
+USE employee_DB
+
+-- Department Seeds
+INSERT INTO department (deparment_name) VALUES ("Accounting");
+INSERT INTO department (deparment_name) VALUES ("Marketing");
+INSERT INTO department (deparment_name) VALUES ("Human Resources");
+INSERT INTO department (deparment_name) VALUES ("Technology");
+
+-- 
+INSERT INTO role_employee (flavor, price, quantity)
+-- VALUES ("chocolate", 3.10, 120);
+
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("strawberry", 3.25, 75);
