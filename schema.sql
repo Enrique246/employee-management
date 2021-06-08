3fa6d1e07ab7901b5b975243e11fe4de213571ff
Henri <ecch9210@gmail.com>
2021-06-07 19:19:14 -0500

    SQL finished

--- schema.sql
+++ schema.sql
@@ -4,23 +4,26 @@ CREATE database employee_DB;
 USE employee_DB;
 
 CREATE TABLE department (   
-    id INT PRIMARY KEY AUTO_INCREMENT, 
-    deparment_name VARCHAR(30)
-)
+    id INT PRIMARY KEY AUTO_INCREMENT Not Null, 
+    deparment_name VARCHAR(30) Not Null,
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE role_employee (   
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL, 
-    department_id INT 
-)
+    id INT PRIMARY KEY AUTO_INCREMENT Not Null,
+    title VARCHAR(30) Not Null,
+    salary DECIMAL Not Null, 
+    department_id INT Not Null, 
+    PRIMARY KEY (id)
+);
 CREATE TABLE employee (   
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT 
-)
+    id INT PRIMARY KEY AUTO_INCREMENT Not Null,
+    first_name VARCHAR(30)Not Null,
+    last_name VARCHAR(30)Not Null,
+    role_id INT Not Null,
+    manager_id INT Not Null, 
+    PRIMARY KEY (id)
+);
 
 
 SELECT * FROM deparment;
--- seeds.sql
+++ seeds.sql
@@ -1,14 +1,17 @@
 USE employee_DB
 
 -- Department Seeds
-INSERT INTO department (deparment_name) VALUES ("Accounting");
-INSERT INTO department (deparment_name) VALUES ("Marketing");
-INSERT INTO department (deparment_name) VALUES ("Human Resources");
-INSERT INTO department (deparment_name) VALUES ("Technology");
+INSERT INTO department (id,deparment_name) VALUES ("1","Accounting");
+INSERT INTO department (id,deparment_name) VALUES ("2","Marketing");
+INSERT INTO department (id,deparment_name) VALUES ("3","Human Resources");
 
--- 
-INSERT INTO role_employee (flavor, price, quantity)
--- VALUES ("chocolate", 3.10, 120);
+-- Role Seeds
+INSERT INTO role_employee (id, title, salary, department_id) VALUES ("1","ACCOUNTANT","3000","1");
+INSERT INTO role_employee (id, title, salary, department_id) VALUES ("2","DEVELOPER","4000","2");
+INSERT INTO role_employee (id, title, salary, department_id) VALUES ("3","RECRUITER","5000","3");
 
--- INSERT INTO products (flavor, price, quantity)
--- VALUES ("strawberry", 3.25, 75);
+-- Employee Seeds
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES ("1","Sofia","Hernandez","1","1");
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES ("2","Enrique","Fernandez","2","2");
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES ("3","Esteban","Hernandez","3","3");
+
